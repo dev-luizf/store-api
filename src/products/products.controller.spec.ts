2d7c358c92774fe3c5792a2ea86d42c332fbcf79
dev-luizf <luizfurtadomkt@hotmail.com>
2023-04-05 12:02:33 -0300

    test: implementa testes unitários no módulo de produtos

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -1,20 +1,117 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import ProductSeed from '../../seed';
+import { Product } from './interfaces/product.interface';
+import { ObjectId } from 'mongoose';
+import { BadRequestException } from '@nestjs/common';
+
+const productList = ProductSeed as (Product & { _id: ObjectId })[];
+const newProduct = productList[0];
+const invalidIdError = 'id is invalid';
+const requiredIdError = 'id is required';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
+  let service: ProductsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
-      providers: [ProductsService],
+      providers: [
+        {
+          provide: ProductsService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
+    service = module.get<ProductsService>(ProductsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('deve retornar uma lista de produtos', async () => {
+      jest.spyOn(service, 'findAll').mockResolvedValue(productList);
+      expect(await controller.findAll()).toEqual(productList);
+    });
+  });
+
+  describe('findOne', () => {
+    it('deve retornar o produto especificado', async () => {
+      jest.spyOn(service, 'findOne').mockResolvedValue(productList[0]);
+      expect(await controller.findOne('63fc13c7b817f1ef5af52631')).toEqual(
+        productList[0],
+      );
+    });
+
+    it('deve retornar um erro se o id não for valido', async () => {
+      try {
+        await controller.findOne('11111111111');
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(error.response.message).toEqual(invalidIdError);
+      }
+    });
+
+    it('deve retornar um erro se o id não for informado', async () => {
+      const id = undefined;
+      try {
+        await controller.findOne(id);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(error.response.message).toEqual(requiredIdError);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('deve criar um novo produto', async () => {
+      jest.spyOn(service, 'create').mockResolvedValue(productList[0]);
+      expect(await controller.create(newProduct)).toEqual(productList[0]);
+    });
+
+    it('deve retornar um erro se as informações não forem válidas', async () => {
+      try {
+        await controller.create({} as Product);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(error.response.error).toEqual('Bad Request');
+      }
+    });
+  });
+
+  describe('delete', () => {
+    it('deve excluir um produto existente', async () => {
+      jest.spyOn(service, 'remove').mockResolvedValue({ deletedCount: 1, acknowledged: true });
+      await expect(
+        controller.remove('63fc13c7b817f1ef5af52631'),
+      ).resolves.not.toThrow();
+    });
+
+    it('deve retornar um erro se o id não for valido', async () => {
+      try {
+        await controller.remove('11111111111');
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(error.response.message).toEqual(invalidIdError);
+      }
+    });
+
+    it('deve retornar um erro se o id não for informado', async () => {
+      const id = undefined;
+      try {
+        await controller.remove(id);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(error.response.message).toEqual(requiredIdError);
+      }
+    });
   });
 });
--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -1,18 +1,120 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import ProductSeed from '../../seed';
+import { Product } from './interfaces/product.interface';
+import { Model } from 'mongoose';
+import { NotFoundException } from '@nestjs/common';
+
+const productList = [
+  { 
+    _id: '63fc13c7b817f1ef5af52631',
+    ...ProductSeed[0],
+  },
+  {
+    _id: '63fc13c7b817f1ef5af52632',
+    ...ProductSeed[1],
+  }
+];
+const notFoundError = 'product not found';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  let model: Model<Product>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        {
+          provide: 'PRODUCT_MODEL',
+          useValue: {
+            create: jest.fn().mockResolvedValue(productList[0]),
+            find: jest.fn().mockResolvedValue(productList),
+            findOne: jest.fn().mockResolvedValue(null),
+            findById: jest.fn().mockResolvedValue(productList[0]),
+            updateOne: jest.fn().mockResolvedValue(null),
+            deleteOne: jest.fn().mockResolvedValue(null),
+          },
+        },
+      ],
     }).compile();
 
+    model = module.get<Model<Product>>('PRODUCT_MODEL');
     service = module.get<ProductsService>(ProductsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('create', () => {
+    it('deve criar um novo produto', async () => {
+      const newProduct = await service.create(productList[0]);
+      expect(newProduct._id).toEqual(productList[0]._id);
+    });
+  });
+
+  describe('findAll', () => {
+    it('deve retornar uma lista de produtos', async () => {
+      const list = await service.findAll();
+      expect(list).toEqual(productList);
+    });
+  });
+
+  describe('findOne', () => {
+    it('deve retornar um produto específico', async () => {
+      const product = await service.findOne(productList[0]._id);
+      expect(product).toEqual(productList[0]);
+    });
+
+    it('deve lançar um erro se o produto não for encontrado', async () => {
+      jest
+        .spyOn(model, 'findById')
+        .mockReturnValue(null);
+      try {
+        await service.findOne(productList[0]._id);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.response.message).toEqual(notFoundError);
+      }
+    });
+  });
+
+  describe('update', () => {
+    const product = productList[0];
+
+    it('deve atualizar um produto', async () => {
+      await expect(
+        service.update(product._id, { name: 'novo nome' }),
+      ).resolves.not.toThrow();
+    });
+
+    it('deve lançar um erro se o produto não for encontrado', async () => {
+      jest
+        .spyOn(model, 'findById')
+        .mockReturnValue(null);
+      try {
+        await service.update(product._id, product);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.response.message).toEqual(notFoundError);
+      }
+    });
+  });
+
+  describe('remove', () => {
+    it('deve remover um produto', async () => {
+      await expect(
+        service.remove(productList[0]._id),
+      ).resolves.not.toThrow();
+    });
+
+    it('deve lançar um erro se o produto não for encontrado', async () => {
+      jest
+        .spyOn(model, 'findById')
+        .mockReturnValue(null);
+      try {
+        await service.remove(productList[0]._id);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.response.message).toEqual(notFoundError);
+      }
+    });
   });
 });
