a0c585290efd712dca81035cb48c77614bd6a8c6
dev-luizf <luizfurtadomkt@hotmail.com>
2023-04-05 11:12:23 -0300

    fix: corrige tipagem de preço

--- src/products/interfaces/product.interface.ts
+++ src/products/interfaces/product.interface.ts
@@ -2,7 +2,7 @@ import { Document } from 'mongoose';
 
 export interface Product extends Document {
   readonly name: string;
-  readonly price: string;
+  readonly price: number;
   readonly category: string;
   readonly thumbnail: string;
   readonly description: string;
