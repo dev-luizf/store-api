d669b7b8637781929b370651eb95ca518bd24a66
dev-luizf <luizfurtadomkt@hotmail.com>
2023-04-05 12:56:58 -0300

    docs: implementa documentação com swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const PORT = parseInt(process.env.PORT) || 3001;
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Store Manager API')
+    .setDescription(
+      'Uma API para controle de uma lista produtos utilizando nest.js, mongoose, joi, swagger, jest e docker.',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
 }
 bootstrap();
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,7 +4,10 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { joiValidate } from 'src/utils/joiValidate';
 import { createProductSchema, updateProductSchema } from 'src/joi-schemas/product.joischema';
+import { ApiBody, ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import ProductsSeed from '../seed';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
@@ -14,23 +17,96 @@ export class ProductsController {
     if (id.length !== 24) throw new BadRequestException('id is invalid');
   }
 
+  @ApiOperation({ description: 'Insere um novo produto no banco de dados.' })
+  @ApiCreatedResponse({
+    description: 'Produto cadastrado com sucesso.',
+    schema: {
+      example: {
+        _id: '5f9f1b9b9c9d0b1b8c8b9c9d',
+        ...ProductsSeed[0],
+        __v: 0,
+      },
+    },
+  })
+  @ApiBody({
+    description: 'Produto a ser cadastrado.',
+    type: CreateProductDto,
+    examples: {
+      'Formato válido': {
+        value: ProductsSeed[0],
+      },
+    },
+  })
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     joiValidate(createProductSchema, createProductDto)
     return this.productsService.create(createProductDto);
   }
 
+  @ApiOperation({
+    description: 'Retorna uma lista de produtos.',
+  })
+  @ApiOkResponse({
+    description: 'Lista de produtos.',
+    schema: {
+      example: [
+        {
+          _id: '5f9f1b9b9c9d0b1b8c8b9c9d',
+          ...ProductsSeed[0],
+          __v: 0,
+        },
+      ],
+    },
+  })
   @Get()
   findAll() {
     return this.productsService.findAll();
   }
 
+  @ApiOperation({
+    description: 'Retornar um produto específico.',
+  })
+  @ApiParam({
+    name: 'id',
+    examples: {
+      'Formato válido': {
+        value: '642d90c6c6949b2ad52dd459',
+      },
+    },
+  })
+  @ApiOkResponse({
+    description: 'Um produto específico.',
+    schema: {
+      example: {
+        _id: '5f9f1b9b9c9d0b1b8c8b9c9d',
+        ...ProductsSeed[0],
+        __v: 0,
+      },
+    },
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     this.validateId(id);
     return this.productsService.findOne(id);
   }
 
+  @ApiOperation({ description: 'Atualiza um produto.' })
+  @ApiParam({
+    name: 'id',
+    examples: {
+      'Formato válido': {
+        value: '642d90c6c6949b2ad52dd459',
+      },
+    },
+  })
+  @ApiBody({
+    description: 'Informação a ser atualizada.',
+    examples: {
+      'Formato válido': {
+        value: { price: 99.99 },
+      },
+    },
+  })
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     this.validateId(id);
@@ -38,6 +114,15 @@ export class ProductsController {
     await this.productsService.update(id, updateProductDto);
   }
 
+  @ApiOperation({ description: 'Deleta um produto do banco de dados.' })
+  @ApiParam({
+    name: 'id',
+    examples: {
+      'Formato válido': {
+        value: '642d90c6c6949b2ad52dd459',
+      },
+    },
+  })
   @Delete(':id')
   async remove(@Param('id') id: string) {
     this.validateId(id);
